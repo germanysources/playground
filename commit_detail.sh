834c477bb02356357b1eebef739795544c884692
Johannes Gerbershagen <johannes.gerbershagen@kabelmail.de>
2021-06-14 21:00:56 +0000

    try publish

--- commit_detail.sh
+++ commit_detail.sh
@@ -1,4 +1,4 @@
-$command = '(printf "commit %s\0" $(git cat-file commit HEAD | wc -c); git cat-file commit HEAD)'
+$command '(printf "commit %s\0" $(git cat-file commit HEAD | wc -c); git cat-file commit HEAD)'
 sh -c '$command'
 sh -c '$command | sha1sum'
 
